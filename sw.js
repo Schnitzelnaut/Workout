881103608274680eaa5f79c194417634d1745089
Schnitzelnaut <115431013+Schnitzelnaut@users.noreply.github.com>
2026-06-10 09:11:21 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const VERSION = 'v3';
+const CACHE = 'workout-' + VERSION;
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/Workout/', '/Workout/index.html', '/Workout/manifest.json'])).catch(() => {}));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
